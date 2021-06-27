674ad47c86bf05d9f63771cd89afba03f659ddcf
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-06-27 17:11:44 +0200

    Clean Up Your Project with Modules

---
+++ auth.js
@@ -0,0 +1,29 @@
+module.exports = (app, myDataBase)=>{
+    const passport = require("passport");
+    const bcrypt = require("bcrypt");
+    const { ObjectID } = require('mongodb');
+    const LocalStrategy = require('passport-local');
+
+    // Serialisation and deserealisation here... 
+    passport.serializeUser((user,done)=>{
+        done(null, user._id);
+    })
+
+    passport.deserializeUser((id, done)=>{
+    myDataBase.findOne({_id: new ObjectID(id)}, (err, doc)=>{
+    done(null, doc);
+    })
+    })
+
+    //Strategy 
+
+  passport.use(new LocalStrategy((username, password, done)=>{
+    myDataBase.findOne({username: username}, (err, user)=>{
+      console.log("User "+username+" attempted to log in");
+      if(err){return done(err)};
+      if(!user){return done(null, false);}
+      if(!bcrypt.compareSync(password, user.password)){return done(null, false)}
+      return done(null, user)
+    })
+  }))
+}
---
+++ routes.js
@@ -0,0 +1,74 @@
+module.exports = (app, myDataBase) =>{
+    const passport = require("passport");
+    const bcrypt = require("bcrypt");
+
+
+    //root route
+  app.route("/").get((req,res)=>{
+    res.render("pug", {
+      title: "Connected to Database",
+      message:"Please login",
+      showLogin: true,
+      showRegistration:true
+    }) 
+  })
+
+  //register route
+  app.route('/register').post((req, res, next)=>{
+    const hash = bcrypt.hashSync(req.body.password, 12);
+    myDataBase.findOne({username: req.body.username}, (err, user)=>{
+      if(err){next(err);}
+      else if(user){
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username, password: hash
+        }, (err, doc)=>{
+          if(err){res.redirect('/')}
+          else{
+            next(null, doc.ops[0]);
+          }
+        })
+      }
+    })
+  }, passport.authenticate("local", {faillureRedirect: '/'}), (req, res, next)=>{
+    res.redirect("/profile");
+  }
+  )
+
+
+  //login route
+  app.post("/login", passport.authenticate('local', { failureRedirect: '/'}), 
+  (req,res)=>{
+     res.redirect("/profile");
+  })
+
+  //middleware to check aunth
+  const ensureAuthenticated = (req, res, next) => {
+    if(req.isAuthenticated()){
+      return next();
+    }else{
+      res.redirect("/")
+    }
+  }
+
+  //profile route
+  app.get("/profile", ensureAuthenticated, (req,res)=>{
+    res.render(process.cwd()+"/views/pug/profile", {username: req.user.username})
+  })
+
+  //logout route
+  app.get("/logout", (req,res)=>{
+    req.logout();
+    res.redirect('/');
+  })
+
+  //404
+  app.use((req, res, next)=>{
+    res.status(404).type("text").send("Not Found");
+  })
+
+
+
+
+}
